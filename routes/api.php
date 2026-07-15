b2e5ebe7f97bf91bce3d549f9915c45539664c86
amir jahan <amirjahan454@gmail.com>
2026-07-15 08:03:53 -0700

    Debug::->add api

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::apiResource('users', UserController::class);
+
+
+
+// // ۱. دریافت لیست کاربران
+// Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+// // ۲. ساخت کاربر جدید
+// Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+// // ۳. ویرایش کاربر (هم PUT و هم PATCH پشتیبانی می‌شود)
+// Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+ 
+// // ۴. حذف کاربر
+// Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+// // (اختیاری) ۵. دریافت اطلاعات یک کاربر خاص
+// // Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
